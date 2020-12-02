d5ff74bab2647f1953a3549eca1b8379026fd5d1
Alexey Demyanchuk <alexeydemyanchuk@gmail.com>
2020-12-02 14:38:01 +0100

    Formatted with black

--- trainer.py
+++ trainer.py
@@ -9,25 +9,47 @@ from utils.helpers import colorize_mask
 from utils.metrics import eval_metrics, AverageMeter
 from tqdm import tqdm
 
+
 class Trainer(BaseTrainer):
-    def __init__(self, model, loss, resume, config, train_loader, val_loader=None, train_logger=None, prefetch=True):
-        super(Trainer, self).__init__(model, loss, resume, config, train_loader, val_loader, train_logger)
-        
-        self.wrt_mode, self.wrt_step = 'train_', 0
-        self.log_step = config['trainer'].get('log_per_iter', int(np.sqrt(self.train_loader.batch_size)))
-        if config['trainer']['log_per_iter']: self.log_step = int(self.log_step / self.train_loader.batch_size) + 1
+    def __init__(
+        self,
+        model,
+        loss,
+        resume,
+        config,
+        train_loader,
+        val_loader=None,
+        train_logger=None,
+        prefetch=True,
+    ):
+        super(Trainer, self).__init__(
+            model, loss, resume, config, train_loader, val_loader, train_logger
+        )
+
+        self.wrt_mode, self.wrt_step = "train_", 0
+        self.log_step = config["trainer"].get(
+            "log_per_iter", int(np.sqrt(self.train_loader.batch_size))
+        )
+        if config["trainer"]["log_per_iter"]:
+            self.log_step = int(self.log_step / self.train_loader.batch_size) + 1
 
         self.num_classes = self.train_loader.dataset.num_classes
 
         # TRANSORMS FOR VISUALIZATION
-        self.restore_transform = transforms.Compose([
-            local_transforms.DeNormalize(self.train_loader.MEAN, self.train_loader.STD),
-            transforms.ToPILImage()])
-        self.viz_transform = transforms.Compose([
-            transforms.Resize((400, 400)),
-            transforms.ToTensor()])
-        
-        if self.device ==  torch.device('cpu'): prefetch = False
+        self.restore_transform = transforms.Compose(
+            [
+                local_transforms.DeNormalize(
+                    self.train_loader.MEAN, self.train_loader.STD
+                ),
+                transforms.ToPILImage(),
+            ]
+        )
+        self.viz_transform = transforms.Compose(
+            [transforms.Resize((400, 400)), transforms.ToTensor()]
+        )
+
+        if self.device == torch.device("cpu"):
+            prefetch = False
         if prefetch:
             self.train_loader = DataPrefetcher(train_loader, device=self.device)
             self.val_loader = DataPrefetcher(val_loader, device=self.device)
@@ -35,34 +57,36 @@ class Trainer(BaseTrainer):
         torch.backends.cudnn.benchmark = True
 
     def _train_epoch(self, epoch):
-        self.logger.info('\n')
-            
+        self.logger.info("\n")
+
         self.model.train()
-        if self.config['arch']['args']['freeze_bn']:
-            if isinstance(self.model, torch.nn.DataParallel): self.model.module.freeze_bn()
-            else: self.model.freeze_bn()
-        self.wrt_mode = 'train'
+        if self.config["arch"]["args"]["freeze_bn"]:
+            if isinstance(self.model, torch.nn.DataParallel):
+                self.model.module.freeze_bn()
+            else:
+                self.model.freeze_bn()
+        self.wrt_mode = "train"
 
         tic = time.time()
         self._reset_metrics()
         tbar = tqdm(self.train_loader, ncols=130)
         for batch_idx, (data, target) in enumerate(tbar):
             self.data_time.update(time.time() - tic)
-            #data, target = data.to(self.device), target.to(self.device)
-            self.lr_scheduler.step(epoch=epoch-1)
+            # data, target = data.to(self.device), target.to(self.device)
+            self.lr_scheduler.step(epoch=epoch - 1)
 
             # LOSS & OPTIMIZE
             self.optimizer.zero_grad()
             output = self.model(data)
-            if self.config['arch']['type'][:3] == 'PSP':
+            if self.config["arch"]["type"][:3] == "PSP":
                 assert output[0].size()[2:] == target.size()[1:]
-                assert output[0].size()[1] == self.num_classes 
+                assert output[0].size()[1] == self.num_classes
                 loss = self.loss(output[0], target)
                 loss += self.loss(output[1], target) * 0.4
                 output = output[0]
             else:
                 assert output.size()[2:] == target.size()[1:]
-                assert output.size()[1] == self.num_classes 
+                assert output.size()[1] == self.num_classes
                 loss = self.loss(output, target)
 
             if isinstance(self.loss, torch.nn.DataParallel):
@@ -78,49 +102,60 @@ class Trainer(BaseTrainer):
             # LOGGING & TENSORBOARD
             if batch_idx % self.log_step == 0:
                 self.wrt_step = (epoch - 1) * len(self.train_loader) + batch_idx
-                self.writer.add_scalar(f'{self.wrt_mode}/loss', loss.item(), self.wrt_step)
+                self.writer.add_scalar(
+                    f"{self.wrt_mode}/loss", loss.item(), self.wrt_step
+                )
 
             # FOR EVAL
             seg_metrics = eval_metrics(output, target, self.num_classes)
             self._update_seg_metrics(*seg_metrics)
             pixAcc, mIoU, _ = self._get_seg_metrics().values()
-            
+
             # PRINT INFO
-            tbar.set_description('TRAIN ({}) | Loss: {:.3f} | Acc {:.2f} mIoU {:.2f} | B {:.2f} D {:.2f} |'.format(
-                                                epoch, self.total_loss.average, 
-                                                pixAcc, mIoU,
-                                                self.batch_time.average, self.data_time.average))
+            tbar.set_description(
+                "TRAIN ({}) | Loss: {:.3f} | Acc {:.2f} mIoU {:.2f} | B {:.2f} D {:.2f} |".format(
+                    epoch,
+                    self.total_loss.average,
+                    pixAcc,
+                    mIoU,
+                    self.batch_time.average,
+                    self.data_time.average,
+                )
+            )
 
         # METRICS TO TENSORBOARD
         seg_metrics = self._get_seg_metrics()
-        for k, v in list(seg_metrics.items())[:-1]: 
-            self.writer.add_scalar(f'{self.wrt_mode}/{k}', v, self.wrt_step)
+        for k, v in list(seg_metrics.items())[:-1]:
+            self.writer.add_scalar(f"{self.wrt_mode}/{k}", v, self.wrt_step)
         for i, opt_group in enumerate(self.optimizer.param_groups):
-            self.writer.add_scalar(f'{self.wrt_mode}/Learning_rate_{i}', opt_group['lr'], self.wrt_step)
-            #self.writer.add_scalar(f'{self.wrt_mode}/Momentum_{k}', opt_group['momentum'], self.wrt_step)
+            self.writer.add_scalar(
+                f"{self.wrt_mode}/Learning_rate_{i}", opt_group["lr"], self.wrt_step
+            )
+            # self.writer.add_scalar(f'{self.wrt_mode}/Momentum_{k}', opt_group['momentum'], self.wrt_step)
 
         # RETURN LOSS & METRICS
-        log = {'loss': self.total_loss.average,
-                **seg_metrics}
+        log = {"loss": self.total_loss.average, **seg_metrics}
 
-        #if self.lr_scheduler is not None: self.lr_scheduler.step()
+        # if self.lr_scheduler is not None: self.lr_scheduler.step()
         return log
 
     def _valid_epoch(self, epoch):
         if self.val_loader is None:
-            self.logger.warning('Not data loader was passed for the validation step, No validation is performed !')
+            self.logger.warning(
+                "Not data loader was passed for the validation step, No validation is performed !"
+            )
             return {}
-        self.logger.info('\n###### EVALUATION ######')
+        self.logger.info("\n###### EVALUATION ######")
 
         self.model.eval()
-        self.wrt_mode = 'val'
+        self.wrt_mode = "val"
 
         self._reset_metrics()
         tbar = tqdm(self.val_loader, ncols=130)
         with torch.no_grad():
             val_visual = []
             for batch_idx, (data, target) in enumerate(tbar):
-                #data, target = data.to(self.device), target.to(self.device)
+                # data, target = data.to(self.device), target.to(self.device)
                 # LOSS
                 output = self.model(data)
                 loss = self.loss(output, target)
@@ -139,9 +174,11 @@ class Trainer(BaseTrainer):
 
                 # PRINT INFO
                 pixAcc, mIoU, _ = self._get_seg_metrics().values()
-                tbar.set_description('EVAL ({}) | Loss: {:.3f}, PixelAcc: {:.2f}, Mean IoU: {:.2f} |'.format( epoch,
-                                                self.total_loss.average,
-                                                pixAcc, mIoU))
+                tbar.set_description(
+                    "EVAL ({}) | Loss: {:.3f}, PixelAcc: {:.2f}, Mean IoU: {:.2f} |".format(
+                        epoch, self.total_loss.average, pixAcc, mIoU
+                    )
+                )
 
             # WRTING & VISUALIZING THE MASKS
             val_img = []
@@ -149,24 +186,25 @@ class Trainer(BaseTrainer):
             for d, t, o in val_visual:
                 d = self.restore_transform(d)
                 t, o = colorize_mask(t, palette), colorize_mask(o, palette)
-                d, t, o = d.convert('RGB'), t.convert('RGB'), o.convert('RGB')
+                d, t, o = d.convert("RGB"), t.convert("RGB"), o.convert("RGB")
                 [d, t, o] = [self.viz_transform(x) for x in [d, t, o]]
                 val_img.extend([d, t, o])
             val_img = torch.stack(val_img, 0)
             val_img = make_grid(val_img.cpu(), nrow=3, padding=5)
-            self.writer.add_image(f'{self.wrt_mode}/inputs_targets_predictions', val_img, self.wrt_step)
+            self.writer.add_image(
+                f"{self.wrt_mode}/inputs_targets_predictions", val_img, self.wrt_step
+            )
 
             # METRICS TO TENSORBOARD
             self.wrt_step = (epoch) * len(self.val_loader)
-            self.writer.add_scalar(f'{self.wrt_mode}/loss', self.total_loss.average, self.wrt_step)
+            self.writer.add_scalar(
+                f"{self.wrt_mode}/loss", self.total_loss.average, self.wrt_step
+            )
             seg_metrics = self._get_seg_metrics()
-            for k, v in list(seg_metrics.items())[:-1]: 
-                self.writer.add_scalar(f'{self.wrt_mode}/{k}', v, self.wrt_step)
+            for k, v in list(seg_metrics.items())[:-1]:
+                self.writer.add_scalar(f"{self.wrt_mode}/{k}", v, self.wrt_step)
 
-            log = {
-                'val_loss': self.total_loss.average,
-                **seg_metrics
-            }
+            log = {"val_loss": self.total_loss.average, **seg_metrics}
 
         return log
 
@@ -190,5 +228,5 @@ class Trainer(BaseTrainer):
         return {
             "Pixel_Accuracy": np.round(pixAcc, 3),
             "Mean_IoU": np.round(mIoU, 3),
-            "Class_IoU": dict(zip(range(self.num_classes), np.round(IoU, 3)))
+            "Class_IoU": dict(zip(range(self.num_classes), np.round(IoU, 3))),
         }
